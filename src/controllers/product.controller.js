1ab58e511b3b9f631c7f26728c12e41216553587
Labib1851 <labibabeed2007@gmail.com>
2026-07-25 08:51:14 +0600

    rem console

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,48 +1,6 @@
 
 import productService from "../services/product.service.js";
-
-// product add control
-// const addProduct = async (req,res,next) => {
-//     try {
-//         const productData =  {...req.body};
-
-//         if(typeof productData.sizes === "string") productData.sizes = JSON.parse(productData.sizes);
-//         if(typeof productData.colors === "string") productData.colors = JSON.parse(productData.colors);
-
-//          //set main image path
-//          if(req.files && req.files["mainImage"]) {
-//             productData.mainImage = `/uploads/${req.files["mainImage"][0].filename}`;
-
-//          }else {
-//             return res.status(400).json({success:false, message: "Main image is"})
-//          }
-
-//          // all gallery image file path set the array
-//          if(req.files && req.files["galleryImages"]) {
-//             productData.galleryImages = req.files["galleryImages"].map(
-//                 (file) => `/uploads/${file.filename}`
-//             );
-//          } else{
-//             productData.galleryImages = [];
-//          }
-         
  
-
-//         // SKU duplicate check
-//         const newProduct = await productService.createProduct(productData);
-
-//         res.status(201).json({
-//             success: true,
-//             message: "Product published successfully with images! ",
-//             data: newProduct,
-//         });    
-//     } catch (error) {
-//         next(error);
-//     }
-// };
-
-
-// product add control (Cloudinary Support)
 const addProduct = async (req, res, next) => {
   try {
     const productData = { ...req.body };
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -20,30 +20,7 @@ const productSchema = new mongoose.Schema(
     {timestamp: true}
 );
 
-// module.exports = mongoose.model("Product", productSchema)
+ 
 export default mongoose.model("Product",productSchema)
 
-
-// import mongoose from 'mongoose';
-
-// const productSchema = new mongoose.Schema(
-//     {
-//         title: { type: String, required: true, trim: true },
-//         sku: { type: String, required: true, unique: true, trim: true },
-//         price: { type: Number, required: true },
-//         category: { type: String, required: true },
-//         fabric: { type: String },
-//         shirtDetails: { type: String },
-//         dupattaDetails: { type: String },
-//         trouserDetails: { type: String },
-//         sizes: [{ type: String }], 
-//         colors: [{ type: String }], 
-//         inStock: { type: Boolean, default: true },
-//         mainImage: { type: String, required: true }, 
-//         galleryImage: [{ type: String }] 
-//     }, 
-//     { timestamps: true } // Fixed: added 's'
-// );
-
-// // Fixed: Added the model name "Product"
-// export default mongoose.model("Product", productSchema);
+ 
